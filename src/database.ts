99fba3d11b997b94073ccc1e6c0dbe7ceca0c66a
Praksix <nathan.vianey@gmail.com>
2024-12-18 11:49:29 +0100

    petite corrrection d'un micro problème

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,10 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   user: 'admin', // Remplacez par votre utilisateur PostgreSQL
-  host: 'localhost',        // Hôte de la base de données
+  host: 'db',        // Hôte de la base de données
   database: 'bdd',   // Nom de la base
   password: 'admin', // Mot de passe PostgreSQL
-  port: 5433                // Port par défaut de PostgreSQL
+  port: 5432                // Port par défaut de PostgreSQL
 });
 
 export const query = (text: string, params?: any[]) => {
