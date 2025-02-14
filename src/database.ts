ba9e81de7555e4b3b4892eaa8c96fdbffc91f751
Praksix <nathan.vianey@gmail.com>
2025-02-14 14:49:50 +0100

    add jsp quoi

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,12 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  user: 'admin', // Remplacez par votre utilisateur PostgreSQL
-  host: 'db',        // Hôte de la base de données
-  database: 'bdd',   // Nom de la base
-  password: 'admin', // Mot de passe PostgreSQL
-  port: 5432                // Port par défaut de PostgreSQL
+  // user: 'admin', // Remplacez par votre utilisateur PostgreSQL
+  // host: 'db',        // Hôte de la base de données
+  // database: 'bdd',   // Nom de la base
+  // password: 'admin', // Mot de passe PostgreSQL
+  // port: 5432                // Port par défaut de PostgreSQL
+  connectionString: process.env.DATABASE_URL,
 });
 
 export const query = (text: string, params?: any[]) => {
